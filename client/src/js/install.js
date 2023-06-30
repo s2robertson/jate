414c609c2697020a408881b68276efa1fdd4314d
Steven Robertson <s2robertson@gmail.com>
2023-06-29 21:25:37 -0400

    added installation code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+(function handleInstallPrompt() {
+    let installPrompt = null;
+    
+    window.addEventListener('beforeinstallprompt', (event) => {
+        event.preventDefault();
+        installPrompt = event;
+        butInstall.removeAttribute('hidden');
+    });
+    
+    butInstall.addEventListener('click', async () => {
+        if (!installPrompt) {
+            return;
+        }
+        const result = await installPrompt.prompt();
+        console.log(`Install prompt outcome: ${result.outcome}`);
+        // should this code be removed? (see appinstalled)
+        installPrompt = null;
+        butInstall.setAttribute('hidden', 'true');
+    });
+    
+    window.addEventListener('appinstalled', (event) => {
+        installPrompt = null;
+        butInstall.setAttribute('hidden', 'true');
+    });
+})()
